f85b251a4c06446ba1193c33e8ad5f93e23b5975
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-03 13:15:14 +0900

    all files

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -6,7 +6,7 @@
         <?= $this->include ('komponen/sidebar'); ?>
         <div class="col-12 col-sm-9" >
             <h1><?= $content ?></h1>
-            
+        
         </div>   
     </div>
 </div>
