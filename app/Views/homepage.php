8c3ee9e1fb4c6b2ef4407354ff49cd73e165c1f5
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-03 12:31:32 +0900

    tes up lagi

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -7,8 +7,8 @@
         <div class="col-12 col-sm-9" >
             <h1><?= $content ?></h1>
             
-        </div>
+        </div> 
     </div>
 </div>
 <?= $this->endSection() ?>
-
+ 
--- app/Views/llogin.php
+++ app/Views/llogin.php
@@ -16,7 +16,7 @@
 <body><br>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">SIM UMMAT judul</a>
+            <a class="navbar-brand" href="#"><strong>SIM UMMAT judul</strong></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
