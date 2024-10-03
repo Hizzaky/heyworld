8be9a32031b7b930b43294a659f8baac0e8de666
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-03 13:14:26 +0900

    up lagi

--- app/Views/homepage.php
+++ app/Views/homepage.php
@@ -2,7 +2,7 @@
 <?= $this->section('konten') ?>
 <div class="mrg-home">
 
-    <div class="row">
+    <div class="row"> 
         <?= $this->include ('komponen/sidebar'); ?>
         <div class="col-12 col-sm-9" >
             <h1><?= $content ?></h1>
