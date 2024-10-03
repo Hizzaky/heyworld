b7f45f38288050fa9a404fe1bbe57fa04475d211
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-03 12:27:12 +0900

    tes up

--- app/Views/llogin.php
+++ app/Views/llogin.php
@@ -16,7 +16,7 @@
 <body><br>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#"><strong>SIM UMMAT</strong></a>
+            <a class="navbar-brand" href="#">SIM UMMAT judul</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -33,7 +33,7 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
+                            Dropdown.
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="#">Sebagai Dosen</a>
