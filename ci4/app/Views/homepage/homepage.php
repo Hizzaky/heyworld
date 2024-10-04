4b32b53af305cab4970c3f63b674c930dc3a953d
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-04 17:31:14 +0900

    homepage

--- ci4/app/Views/homepage/homepage.php
+++ ci4/app/Views/homepage/homepage.php
@@ -1,13 +1,1 @@
-<?= $this->extend('layout/layout_home') ?>
-<?= $this->section('konten') ?>
-<div class="mrg-home">
-
-    <div class="row">
-        <?= $this->include ('../komponen/sidebar'); ?>
-        <div class="col-12 col-sm-9" >
-            <h1><?= $content ?></h1>
-            
-        </div>
-    </div>
-</div>
-<?= $this->endSection() ?>
+<h1>homepage</h1>
