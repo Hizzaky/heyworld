c856c264bd9751d2c5112ddbf32bac6fbcb097ed
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-04 13:12:54 +0900

    form register

--- ci4/app/Views/daftar/daftar.php
+++ ci4/app/Views/daftar/daftar.php
@@ -6,7 +6,7 @@
     <div class="col-12 col-sm-3">    </div>
     <div class="card col-12 col-sm-6 col-md-6 mrg-login">
         <h3 class="card-header ">
-            <?= $header_title . " " . $kategori ?>
+            <?= $header_title ?>
         </h3>
         <div class="card-body">
             <?php if (isset($validasi)): ?>
