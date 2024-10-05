dc753842805414f40bb3dfe0a715a232ef291c6d
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-05 20:23:01 +0900

    login

--- ci4/app/Views/login/login.php
+++ ci4/app/Views/login/login.php
@@ -1,7 +1,6 @@
 <?= $this->extend('layout/layout_home') ?>
 
 <?= $this->section('konten') ?>
-<?php $session=session(); ?>
 <div class="row">
     <div class="col-12 col-sm-3"> </div>
     <div class="card col-12 col-sm-6 col-md-6 mrg-login">
