2ef0202a17e3e13cba9d09b2a7c7d9560aa6404e
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-05 20:04:27 +0900

    login

--- ci4/app/Views/login/login.php
+++ ci4/app/Views/login/login.php
@@ -9,13 +9,7 @@
             <?= $header_title . " " . $kategori ?>
         </h3>
         <div class="card-body">
-            <?php if (isset($fail)): ?>
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <?= $fail ?>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
-                            aria-hidden="true">&times;</span></button>
-                </div>
-            <?php endif; ?>
+            
             <?php if (isset($validasi)): ?>
                 <div class="text-danger">
                     <?= $validasi->listErrors() ?> <!-- validasi form error -->
