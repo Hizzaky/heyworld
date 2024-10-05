be27722a7e81aff79fed05faf8ee928f63018fcc
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-05 20:22:43 +0900

    login

--- ci4/app/Views/login/login.php
+++ ci4/app/Views/login/login.php
@@ -10,7 +10,7 @@
         </h3>
         <div class="card-body">
             <pre>
-                <?= $session ?>
+                <?= print_r($session) ?>
             </pre>
             <?php if (isset($validasi)): ?>
                 <div class="text-danger">
