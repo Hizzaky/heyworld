96af7de22dbb26e362560892d342ce44246f9400
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-06 23:01:15 +0900

    homepage dosen

--- ci4/app/Views/layout/dosen/main.php
+++ ci4/app/Views/layout/dosen/main.php
@@ -101,13 +101,7 @@
         <div class="container"><br>
             <div class="card card-konten">
                 <div class="card-body">
-                    <!-- <h5 class="card-title">Card title</h5> -->
-                    <!-- <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6> -->
-                    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the -->
-                        <!-- card's content.</p> -->
                         <?= $this->renderSection('konten') ?>
-                    <!-- <a href="#" class="card-link">Card link</a> -->
-                    <!-- <a href="#" class="card-link">Another link</a> -->
                 </div>
             </div>
         </div>
