a2e71fc802a26b8df672eab1ee58c29aa4623f37
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-06 21:50:02 +0900

    layout

--- ci4/app/Views/layout/dosen/main.php
+++ ci4/app/Views/layout/dosen/main.php
@@ -38,7 +38,7 @@
 <body>
     <div class="">
         <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding-top:2%">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
                 <a class="navbar-brand" href="/"><strong>SIM UMMAT</strong></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
