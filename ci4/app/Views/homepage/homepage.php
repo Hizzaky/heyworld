f38ee57ff5c8d46475893668cdc7c414a952b5fe
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-07 18:20:07 +0900

    homepage

--- ci4/app/Views/homepage/homepage.php
+++ ci4/app/Views/homepage/homepage.php
@@ -3,7 +3,7 @@
 <div class="mrg-home">
 
     <div class="row">
-        <?= $this->include ('komponen/sidebar'); ?>
+        <?= $this->include ('komponen/sidebarHomepage'); ?>
         <div class="col-12 col-sm-9" >
             <h1><?= $konten ?></h1>
             
