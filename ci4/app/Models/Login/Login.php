39af01bfd645d60a16f87ff5a6e773b387e29c79
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-07 22:31:51 +0900

    login

--- ci4/app/Models/Login/Login.php
+++ ci4/app/Models/Login/Login.php
@@ -107,22 +107,21 @@ class Login extends BaseController
         $tbl = 't_' . lcfirst($jenis_user);
         $res = $model->where1($tbl, $field, $value); // validasi username password
 
-
         if (count($res) == 0) {
             $return['login'] = '0';
-            return $return;
+            // return $return;
         } else {
             if (password_verify($password, $res[0]['password'])) {
                 $return['login'] = '1';
                 $dataUser=$this->userData($res[0],$jenis_user);
 
                 $sesi->set('login', $dataUser);
-                return $return;
+                // return $return;
             } else {
                 $return['login'] = '0';
-                return $return;
             }
         }
+        return $return;
     }
     protected function userData($data,$jenis_user){
         if($jenis_user=='Dosen'){
