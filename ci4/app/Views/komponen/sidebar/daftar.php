d3c491994b4c1fd4b0dbd454911a6d5f7e15c400
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-07 18:48:08 +0900

    homepage

--- ci4/app/Views/komponen/sidebar/daftar.php
+++ ci4/app/Views/komponen/sidebar/daftar.php
@@ -3,7 +3,7 @@
         <li class="list-group-item s-head"><strong>Daftar Akun : </strong></li>
 <div class="row">
     <div class="col-3 col-sm-3">
-        <div class="col-sm-offset-4 col-sm-1">
+        <div class="col-sm-offset-4 col-sm-1"> 
 
             <li class="list-group-item s-body"><a class="btn" href="/Daftar/dosen">Dosen</a></li>
             <li class="list-group-item s-body"><a class="btn" href="/Daftar/prodi">Prodi</a></li>
