7ed83a9d2f84f8eab008e58e3a70736bb99d4a9e
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-07 23:08:16 +0900

    daftar

--- ci4/app/Views/layout/dosen/main.php
+++ ci4/app/Views/layout/dosen/main.php
@@ -50,7 +50,7 @@
         <div class="container">
             <div class="mrg-home"></div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark rnd">
-                <a class="navbar-brand" href="/Dosen"><strong>SIM UMMAT</strong></a>
+                <a class="navbar-brand" href="/"><strong>SIM UMMAT</strong></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -60,7 +60,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Menu <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="/Dashboard/Dosen">Dashboard <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Menu</a>
--- ci4/app/Views/layout/prodi/main.php
+++ ci4/app/Views/layout/prodi/main.php
@@ -52,7 +52,7 @@
         <div class="container">
             <div class="mrg-home"></div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark rnd">
-                <a class="navbar-brand" href="/Dosen"><strong>SIM UMMAT</strong></a>
+                <a class="navbar-brand" href="/"><strong>SIM UMMAT</strong></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -62,7 +62,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#">Menu <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="/Dashboard/Prodi">Dashboard <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Menu</a>
