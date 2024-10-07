9a0ba17666ec1171b0d6b6a2b722e848ebd193b1
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-07 19:19:45 +0900

    homepage

--- ci4/app/Views/komponen/sidebar/daftar.php
+++ ci4/app/Views/komponen/sidebar/daftar.php
@@ -1,19 +1,20 @@
 <div class="">
     <ul class="list-group">
         <li class="list-group-item s-head"><strong>Daftar Akun : </strong></li>
-<div class="row">
-    <div class="col-5"></div>
-    <div class="col-7">
-        <div class="">
+        <div class="row">
+            <div class="col-5"></div>
+            <div class="col-7">
+                <div class="">
 
-            <!-- <li class="list-group-item dropdown-item"><a class="" href="/Daftar/dosen">Dosen</a></li>
+                    <!-- <li class="list-group-item dropdown-item"><a class="" href="/Daftar/dosen">Dosen</a></li>
             <li class="list-group-item dropdown-item"><a class="" href="/Daftar/prodi">Prodi</a></li>
             <li class="list-group-item dropdown-item"><a class="" href="/Daftar/fakultas">Fakulas</a></li> -->
-            <a class="list-group-item btn <?= $sideClass=$jenis_user=='dosen'? 'btn-primary' : 'btn-outline-primary' ?> s-body" href="/Daftar/dosen">Dosen</a>
-            <a class="list-group-item btn btn-outline-primary s-body" href="/Daftar/prodi">Prodi</a>
-            <a class="list-group-item btn btn-outline-primary s-body" href="/Daftar/fakultas">Fakulas</a>
+                    <a class="list-group-item btn <?= $sideClass = $jenis_user == 'dosen' ? 'btn-primary' : 'btn-outline-primary' ?> s-body"
+                        href="/Daftar/dosen">Dosen</a>
+                    <a class="list-group-item btn btn-outline-primary s-body" href="/Daftar/prodi">Prodi</a>
+                    <a class="list-group-item btn btn-outline-primary s-body" href="/Daftar/fakultas">Fakulas</a>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
     </ul>
-</div> 
+</div>
