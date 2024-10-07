f494fdb91e091a2ee8c2bd4e9d6490c860cfa0f9
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-07 18:31:00 +0900

    homepage

--- ci4/app/Views/daftar/dosen.php
+++ ci4/app/Views/daftar/dosen.php
@@ -3,8 +3,10 @@
 <?= $this->section('konten') ?>
 
 <div class="row">
-    <!-- <div class="col-12 col-sm-3"> </div> -->
+    <div class="col-12 col-sm-3"> 
+        
         <?= $this->include('komponen/sidebar/daftar'); ?>
+    </div>
 
     <div class="card col-12 col-sm-6 col-md-6 mrg-konten">
         <h3 class="card-header ">
--- ci4/app/Views/komponen/sidebar/daftar.php
+++ ci4/app/Views/komponen/sidebar/daftar.php
@@ -1,6 +1,6 @@
 <div class="mrg-side">
     <ul class="list-group">
-        <li class="list-group-item s-head">Daftar Akun</li>
+        <li class="list-group-item s-head"><strong>Daftar Akun : </strong></li>
 
         <li class="list-group-item s-body"><a class="btn" href="/Daftar/dosen">Dosen</a></li>
         <li class="list-group-item s-body"><a class="btn" href="/Daftar/prodi">Prodi</a></li>
