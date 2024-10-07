623efa4b8169a77b1130b36a83989a04a5d98816
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-07 18:40:56 +0900

    homepage

--- ci4/app/Views/komponen/sidebar/daftar.php
+++ ci4/app/Views/komponen/sidebar/daftar.php
@@ -1,13 +1,9 @@
 <div class="">
     <ul class="list-group">
         <li class="list-group-item s-head"><strong>Daftar Akun : </strong></li>
-        <div class="row">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-1">
-                <li class="list-group-item dropdown-item"><a class="" href="/Daftar/dosen">Dosen</a></li>
-                <li class="list-group-item s-body dropdown-item"><a class="btn" href="/Daftar/prodi">Prodi</a></li>
-                <li class="list-group-item s-body"><a class="btn" href="/Daftar/fakultas">Fakulas</a></li>
-            </div>
-        </div>
+
+        <li class="list-group-item s-body"><a class="btn" href="/Daftar/dosen">Dosen</a></li>
+        <li class="list-group-item s-body"><a class="btn" href="/Daftar/prodi">Prodi</a></li>
+        <li class="list-group-item s-body"><a class="btn" href="/Daftar/fakultas">Fakulas</a></li>
     </ul>
 </div>
--- ci4/app/Views/layout/homepage/main.php
+++ ci4/app/Views/layout/homepage/main.php
@@ -39,7 +39,7 @@
     }
 
     .s-body {
-        text-align: right;
+        text-align: end;
     }
 </style>
 
