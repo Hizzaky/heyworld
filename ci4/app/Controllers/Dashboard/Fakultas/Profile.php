8dd291d0b429900e04496ab5dcb2e77fea5a88d7
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-08 23:41:24 +0900

    profile

--- ci4/app/Controllers/Dashboard/Fakultas/Profile.php
+++ ci4/app/Controllers/Dashboard/Fakultas/Profile.php
@@ -44,6 +44,8 @@ class Profile extends BaseController
 
                 if($modelTbl){
                     $sesi->setFlashdata('sukses','Update Nama Fakultas Berhasil!');
+                    $sesi->set('nama_user',$_POST['nama_fakultas']);
+                    $data['nama_user']=$_POST['nama_fakultas'];
                 }else{
                     $sesi->setFlashdata('fail','Update Nama Fakultas Gagal!');
                 }
