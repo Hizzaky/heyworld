01c48eacf76414152ea8ed1ed82fd674b5e3b3e4
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-08 11:12:27 +0900

    profil

--- ci4/app/Controllers/Dashboard/Home.php
+++ ci4/app/Controllers/Dashboard/Home.php
@@ -13,6 +13,8 @@ class Home extends BaseController
         $sesi=$dataSesi->get('login');
         // $ret=ucfirst($sesi['jenis_login']);
 
-        return redirect()->to($sesi['jenis_user']);
+        // return redirect()->to($sesi['jenis_user']);
+        echo $sesi['jenis_user'];
+        return '';
     }
 }
