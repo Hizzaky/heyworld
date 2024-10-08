c139981aa19d9c73e637343e665ade2da175c6a2
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-08 10:59:36 +0900

    profil

--- ci4/app/Views/dashboard/fakultas/profile.php
+++ ci4/app/Views/dashboard/fakultas/profile.php
@@ -5,6 +5,7 @@
 <div class="row">
     <div class="col-12 col-sm-3 mrg-side">
         
+         <?= $this->include('komponen/sidebar/profileFakultas'); ?>
     </div>
     <div class="card col-12 col-sm-6 col-md-6 mrg-konten">
         <h3 class="card-header ">
