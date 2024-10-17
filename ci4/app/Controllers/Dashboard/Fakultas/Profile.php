fd73f76bd8ec67986b5ad7210a755c37fd250cd7
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-17 12:18:14 +0900

    update

--- ci4/app/Controllers/Dashboard/Fakultas/Profile.php
+++ ci4/app/Controllers/Dashboard/Fakultas/Profile.php
@@ -14,7 +14,7 @@ class Profile extends BaseController
         $sesi = session();
         $ver = $sesi->get('login');
         if (isset($ver['jenis_user'])) {
-            if ($ver['jenis_user'] != 'Prodi') {
+            if ($ver['jenis_user'] != 'Fakultas') {
                 return redirect()->back();
             }
         } else {
