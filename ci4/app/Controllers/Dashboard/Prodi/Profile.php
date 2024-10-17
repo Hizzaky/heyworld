6d9f4bc656ecc6dc2ef295ab53421093d09777a3
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-17 12:46:42 +0900

    update

--- ci4/app/Controllers/Dashboard/Prodi/Profile.php
+++ ci4/app/Controllers/Dashboard/Prodi/Profile.php
@@ -10,8 +10,19 @@ use App\Models\Dashboard\Prodi\Table\PassModel;
 class Profile extends BaseController
 {
     public function index(){
+        $sesi = session();
+        $data = $sesi->get('login');
+        if (isset($data['jenis_user'])) {
+            if ($data['jenis_user'] != 'Prodi') {
+                // return redirect()->back();
+            }
+        } else {
+            return redirect()->to('/');
+        }
 
-        return 'tes';
+        // return redirect('update-nama');
+        return 'tes again';
+        
     }
 
 
