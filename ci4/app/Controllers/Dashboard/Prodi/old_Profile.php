9669ae1ab5aee4e3b96fe6f36743a3a2804b7655
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-17 12:56:28 +0900

    update

--- ci4/app/Controllers/Dashboard/Prodi/old_Profile.php
+++ ci4/app/Controllers/Dashboard/Prodi/old_Profile.php
@@ -9,8 +9,8 @@ use App\Models\Dashboard\Prodi\Table\PassModel;
 
 class Profile extends BaseController
 {
-    public function index()
-    {
+    
+    public function index(){
         $sesi = session();
         $data = $sesi->get('login');
         if (isset($data['jenis_user'])) {
@@ -60,5 +60,54 @@ class Profile extends BaseController
         }
         return view('dashboard/prodi/profileNama', $data);
     }
+    public function update_password()
+    {
+        helper('form');
+        $sesi = session();
+        $model = new ProfileModel();
+        $modelTbl = new PassModel();
+
+        $data = $this->arData($model->title(), $sesi->get('login'));
+        $data['login'] = $sesi->get('login');
+        $data['side'] = '2';
+
+        if (request()->getMethod() == 'post') {
+            $rules = $model->passRules();
+            if ($this->validate($rules)) {
+                $getData = $modelTbl->find($data['login']['user_id']);
+                
+                if (password_verify($_POST['oldPassword'], $getData['password'])) {
+
+                    $dataTbl = [
+                        'prodi_id' => $getData['prodi_id'],
+                        'password' => $_POST['password']
+                    ];
+                    
+                    $modelTbl->save($dataTbl);
+
+                    if ($modelTbl) {
+                        $sesi->setFlashdata('sukses', 'Update Berhasil!');
+                        // $sesi->setTempdata('sukses', 'Update Berhasil!',3);
+                        $getData = $modelTbl->find($data['login']['user_id']);
+
+                        $dataUser = $this->userData($getData, $data['jenis_user']);
+                        $sesi->set('login', $dataUser);
+                        $data['login'] = $sesi->get('login');
+                    } else {
+                        $sesi->setFlashdata('fail', 'Update Gagal!');
+                    }
+                } else {
+                    $sesi->setFlashdata('fail', 'Update Gagal, Password Terakhir Tidak Sesuai!');
+                }
+
+            } else {
+                $data['validasi'] = $this->validator;
+            }
+        }
+        return view('dashboard/prodi/profilePass', $data);
+    }
+
+
+
 
 }
