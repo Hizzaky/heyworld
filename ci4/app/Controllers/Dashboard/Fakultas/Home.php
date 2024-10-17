a37a825422d8ceabc299c70643759046f294d2b2
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-17 11:12:50 +0900

    update

--- ci4/app/Controllers/Dashboard/Fakultas/Home.php
+++ ci4/app/Controllers/Dashboard/Fakultas/Home.php
@@ -9,19 +9,21 @@ class Home extends BaseController
 {
     public function index()
     {
-        $sesi=session();
+        $sesi = session();
         $this->verPage($sesi->get('login'));
 
-        $model=new Fakultas();
-        $data=$this->arData($model->title(),$sesi->get('login'));
+        $model = new Fakultas();
+        $data = $this->arData($model->title(), $sesi->get('login'));
 
-        return view('dashboard/fakultas/home',$data);
+        return view('dashboard/fakultas/home', $data);
     }
 
-    protected function verPage($data){
+    protected function verPage($data)
+    {
         if (isset($data['jenis_user'])) {
             if ($data['jenis_user'] != 'Fakultas') {
-                redirect()->back();
+                // redirect()->back();
+                redirect()->to('prodii');
             }
         } else {
             redirect()->to('/');
