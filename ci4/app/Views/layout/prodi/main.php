f39d241bd7ee0ad6471f671e0295885e16d51d85
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-17 11:49:05 +0900

    update

--- ci4/app/Views/layout/prodi/main.php
+++ ci4/app/Views/layout/prodi/main.php
@@ -62,7 +62,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="/Dashboard/Prodi">Dashboard <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="/Prodi">Dashboard <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Menu</a>
