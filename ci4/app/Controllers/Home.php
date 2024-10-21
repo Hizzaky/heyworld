fccd659421aac62178e93fa387e954d6a41db9d6
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-21 09:12:37 +0900

    update

--- ci4/app/Controllers/Home.php
+++ ci4/app/Controllers/Home.php
@@ -6,7 +6,6 @@ class Home extends BaseController
 {
     public function index()
     {
-        // return redirect()->to('Homepage/');
-        return 'homepage'; 
+        return redirect()->to('Homepage/');
     }
 }
