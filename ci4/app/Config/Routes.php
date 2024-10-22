abaeb16d1170f6133efc31e7edfd8ffa1e5d00ac
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-23 01:32:10 +0900

    update

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -71,6 +71,7 @@ $routes->group('Fakultas', function ($routes) {
     $routes->add('update_nama', 'Dashboard\Fakultas\Profile::update_nama', ['as' => 'fakultas-update-nama']);
     $routes->add('update_password', 'Dashboard\Fakultas\Profile::update_password',['as'=>'fakultas-update-password']);
     $routes->add('taxbloom', 'Dashboard\Fakultas\cpltb::taxbloom',['as'=>'fakultas-taxbloom']);
+    $routes->add('capaian', 'Dashboard\Fakultas\cpltb::capaian',['as'=>'fakultas-capaian']);
 });
 
 $routes->group('Prodi', function ($routes) {
