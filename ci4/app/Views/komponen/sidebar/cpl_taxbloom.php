cfce6e870a9c3f688464f0be6ee0310497879263
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-23 16:07:36 +0900

    profile

--- ci4/app/Views/komponen/sidebar/cpl_taxbloom.php
+++ ci4/app/Views/komponen/sidebar/cpl_taxbloom.php
@@ -1,6 +1,6 @@
 <div class="">
     <ul class="list-group">
-        <li class="list-group-item s-head "><strong>Menu Profile : </strong></li>
+        <li class="list-group-item s-head "><strong>Menu CPL & Taxonomy Bloom : </strong></li>
         <div class="row">
             <div class="col-5"></div>
             <div class="col-7">
