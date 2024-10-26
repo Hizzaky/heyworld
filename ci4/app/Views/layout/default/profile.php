1648a80aae6c82fbd91d94b3bc89c503dffc178e
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 13:06:48 +0900

    update

--- ci4/app/Views/layout/default/profile.php
+++ ci4/app/Views/layout/default/profile.php
@@ -437,44 +437,29 @@
                         </div>
                     </div>
                     <div class="section-body">
-                        <h2 class="section-title">Hi, Ujang!</h2>
+                        <h2 class="section-title">Hi, <?= $nama_user ?>!</h2>
                         <p class="section-lead">
-                            Change information about yourself on this page.
+                            Kalo dosen pake NIDN Prodi/Fakultas ga perlu
                         </p>
 
                         <div class="row mt-sm-4">
                             <div class="col-12 col-md-12 col-lg-5">
                                 <div class="card profile-widget">
                                     <div class="profile-widget-header">
-                                        <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-1.png"
-                                            class="rounded-circle profile-widget-picture">
-                                        <div class="profile-widget-items">
-                                            <div class="profile-widget-item">
-                                                <div class="profile-widget-item-label">Posts</div>
-                                                <div class="profile-widget-item-value">187</div>
-                                            </div>
-                                            <div class="profile-widget-item">
-                                                <div class="profile-widget-item-label">Followers</div>
-                                                <div class="profile-widget-item-value">6,8K</div>
-                                            </div>
-                                            <div class="profile-widget-item">
-                                                <div class="profile-widget-item-label">Following</div>
-                                                <div class="profile-widget-item-value">2,1K</div>
-                                            </div>
-                                        </div>
+                                        Menu <?= $header_title ?>
                                     </div>
                                     <div class="profile-widget-description">
-                                        <div class="profile-widget-name">Ujang Maman <div
+                                        <!-- <div class="profile-widget-name">Ujang Maman <div
                                                 class="text-muted d-inline font-weight-normal">
                                                 <div class="slash"></div> Web Developer
                                             </div>
-                                        </div>
-                                        Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He
-                                        is not a fictional character but an original hero in my family, a hero for his
-                                        children and for his wife. So, I use the name as a user in this template. Not a
-                                        tribute, I'm just bored with <b>'John Doe'</b>.
+                                        </div> -->
+                                        <a class="list-group-item btn <?= $variable = $side == '1' ? 'active' : 'btn-outline-primary' ?> s-body"
+                                            href="update_nama">Nama Fakultas</a>
+                                        <a class="list-group-item btn <?= $variable = $side == '2' ? 'active' : 'btn-outline-primary' ?> s-body"
+                                            href="update_password">Password</a>
                                     </div>
-                                    <div class="card-footer text-center">
+                                    <!-- <div class="card-footer text-center">
                                         <div class="font-weight-bold mb-2">Follow Ujang On</div>
                                         <a href="#" class="btn btn-social-icon btn-facebook mr-1">
                                             <i class="fab fa-facebook-f"></i>
@@ -488,7 +473,7 @@
                                         <a href="#" class="btn btn-social-icon btn-instagram">
                                             <i class="fab fa-instagram"></i>
                                         </a>
-                                    </div>
+                                    </div> -->
                                 </div>
                             </div>
                             <div class="col-12 col-md-12 col-lg-7">
