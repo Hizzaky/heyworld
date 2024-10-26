e343d1374133fa02b2321ff6c6eec69d53a3fb31
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 16:24:28 +0900

    update

--- ci4/app/Views/layout/default/profile.php
+++ ci4/app/Views/layout/default/profile.php
@@ -489,7 +489,7 @@
                             </div>
                             <div class="col-12 col-md-12 col-lg-7">
                                 <div class="card">
-                                    <form method="post" class="needs-validation" novalidate="">
+                                    <form method="post" class="needs-validation" novalidate>
                                         <div class="card-header">
                                             <h4>Edit <?= $konten.' '.$jenis_user ?></h4>
                                         </div>
@@ -497,7 +497,14 @@
                                             <!-- <div class="row"> -->
                                                 <div class="form-group col-md-12 col-12">
                                                     <label>Nama Fakultas</label>
-                                                    <input type="text" class="form-control" value="<?= $login['nama_user'] ?>" required="">
+                                                    <input type="text" class="form-control" value="<?= $login['nama_user'] ?>" dissabled>
+                                                    <!-- <div class="invalid-feedback">
+                                                        Isi nama fakultas dengan benar!
+                                                    </div> -->
+                                                </div>
+                                                <div class="form-group col-md-12 col-12">
+                                                    <label>Input Nama Baru</label>
+                                                    <input type="text" class="form-control" name="nama_fakultas" value="<?= $login['nama_user'] ?>" required>
                                                     <div class="invalid-feedback">
                                                         Isi nama fakultas dengan benar!
                                                     </div>
@@ -510,7 +517,7 @@
                                                     </div>
                                                 </div> -->
                                             <!-- </div> -->
-                                            <div class="row">
+                                            <!-- <div class="row">
                                                 <div class="form-group col-md-7 col-12">
                                                     <label>Email</label>
                                                     <input type="email" class="form-control" value="ujang@maman.com"
@@ -523,15 +530,15 @@
                                                     <label>Phone</label>
                                                     <input type="tel" class="form-control" value="">
                                                 </div>
-                                            </div>
-                                            <div class="row">
+                                            </div> -->
+                                            <!-- <div class="row">
                                                 <div class="form-group col-12">
                                                     <label>Bio</label>
                                                     <textarea
                                                         class="form-control summernote-simple">Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He is not a fictional character but an original hero in my family, a hero for his children and for his wife. So, I use the name as a user in this template. Not a tribute, I'm just bored with <b>'John Doe'</b>.</textarea>
                                                 </div>
-                                            </div>
-                                            <div class="row">
+                                            </div> -->
+                                            <!-- <div class="row">
                                                 <div class="form-group mb-0 col-12">
                                                     <div class="custom-control custom-checkbox">
                                                         <input type="checkbox" name="remember"
@@ -544,10 +551,10 @@
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div> -->
                                         </div>
                                         <div class="card-footer text-right">
-                                            <button class="btn btn-primary">Save Changes</button>
+                                            <button class="btn btn-primary">Update Nama</button>
                                         </div>
                                     </form>
                                 </div>
