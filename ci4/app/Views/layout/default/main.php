4fdaeeaf8368fd33f1c24d012586f431cd0b4e9b
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 12:45:17 +0900

    update

--- ci4/app/Views/layout/default/main.php
+++ ci4/app/Views/layout/default/main.php
@@ -26,7 +26,7 @@
         <div class="main-wrapper">
             <div class="navbar-bg"></div>
             <nav class="navbar navbar-expand-lg main-navbar">
-                <form class="form-inline mr-auto">
+                <!-- <form class="form-inline mr-auto">
                     <ul class="navbar-nav mr-3">
                         <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
                                     class="fas fa-bars"></i></a></li>
@@ -102,7 +102,7 @@
                             </div>
                         </div>
                     </div>
-                </form>
+                </form> -->
                 <ul class="navbar-nav navbar-right">
                     <li class="dropdown dropdown-list-toggle"><a href="#" data-toggle="dropdown"
                             class="nav-link nav-link-lg message-toggle beep"><i class="far fa-envelope"></i></a>
