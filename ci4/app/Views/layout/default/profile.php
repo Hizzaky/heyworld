0ba0a36ca3d1c7cfcec634d9e18157ff863d6fef
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 18:25:37 +0900

    update

--- ci4/app/Views/layout/default/profile.php
+++ ci4/app/Views/layout/default/profile.php
@@ -27,7 +27,7 @@
             <div class="navbar-bg"></div>
             <nav class="navbar navbar-expand-lg main-navbar">
                 <!-- <form class="form-inline mr-auto"> -->
-                    <ul class="navbar-nav mr-3">
+                    <ul class="navbar-nav mr-3 mr-auto">
                         <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
                     </ul>
                 <!-- </form> -->
