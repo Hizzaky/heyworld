56ffa49c3f0537df2c92ff3a3815db3f3f44fa0b
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 18:34:10 +0900

    update

--- ci4/app/Views/komponen/navbar/default.php
+++ ci4/app/Views/komponen/navbar/default.php
@@ -131,23 +131,24 @@
     </div>
 </li> -->
 <!--  -->
+
 <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
         <img alt="image" src="<?= base_url() ?>/template/assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-        <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+        <div class="d-sm-none d-lg-inline-block"><?= $nama_user ?></div>
     </a>
     <div class="dropdown-menu dropdown-menu-right">
         <div class="dropdown-title">Logged in 5 min ago</div>
-        <a href="features-profile.html" class="dropdown-item has-icon">
+        <a href="Fakultas/Profile" class="dropdown-item has-icon">
             <i class="far fa-user"></i> Profile
         </a>
         <a href="features-activities.html" class="dropdown-item has-icon">
-            <i class="fas fa-bolt"></i> Activities
+            <i class="fas fa-bolt"></i> Menu
         </a>
         <a href="features-settings.html" class="dropdown-item has-icon">
-            <i class="fas fa-cog"></i> Settings
+            <i class="fas fa-cog"></i> Menu
         </a>
         <div class="dropdown-divider"></div>
-        <a href="#" class="dropdown-item has-icon text-danger">
+        <a href="/logUserOut" class="dropdown-item has-icon text-danger">
             <i class="fas fa-sign-out-alt"></i> Logout
         </a>
     </div>
--- ci4/app/Views/layout/default/main.php
+++ ci4/app/Views/layout/default/main.php
@@ -27,8 +27,7 @@
             <div class="navbar-bg"></div>
             <nav class="navbar navbar-expand-lg main-navbar">
                 <ul class="navbar-nav mr-3 mr-auto">
-                    <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a>
-                    </li>
+                    <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
                 </ul>
                 <ul class="navbar-nav navbar-right">
                     <!-- navbar -->
