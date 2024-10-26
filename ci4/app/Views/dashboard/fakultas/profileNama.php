606bd819dffdece419c34ebb485cf03ace313901
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 23:01:45 +0900

    update

--- ci4/app/Views/dashboard/fakultas/profileNama.php
+++ ci4/app/Views/dashboard/fakultas/profileNama.php
@@ -69,48 +69,34 @@
                                 Isi nama fakultas dengan benar!
                             </div>
                         </div>
-                        <!-- <div class="form-group col-md-6 col-12">
-                        <label>Last Name</label>
-                        <input type="text" class="form-control" value="Maman" required="">
-                        <div class="invalid-feedback">
-                            Please fill in the last name
-                        </div>
-                    </div> -->
-                        <!-- </div> -->
-                        <!-- <div class="row">
-                        <div class="form-group col-md-7 col-12">
-                            <label>Email</label>
-                            <input type="email" class="form-control" value="ujang@maman.com" required="">
-                            <div class="invalid-feedback">
-                                Please fill in the email
-                            </div>
-                        </div>
-                        <div class="form-group col-md-5 col-12">
-                            <label>Phone</label>
-                            <input type="tel" class="form-control" value="">
-                        </div>
-                    </div> -->
-                        <!-- <div class="row">
-                        <div class="form-group col-12">
-                            <label>Bio</label>
-                            <textarea
-                                class="form-control summernote-simple">Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He is not a fictional character but an original hero in my family, a hero for his children and for his wife. So, I use the name as a user in this template. Not a tribute, I'm just bored with <b>'John Doe'</b>.</textarea>
-                        </div>
-                    </div> -->
-                        <!-- <div class="row">
-                        <div class="form-group mb-0 col-12">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" name="remember" class="custom-control-input" id="newsletter">
-                                <label class="custom-control-label" for="newsletter">Subscribe
-                                    to newsletter</label>
-                                <div class="text-muted form-text">
-                                    You will get new information about products, offers and
-                                    promotions
-                                </div>
-                            </div>
-                        </div>
-                    </div> -->
                     </div>
+                    <!--  -->
+                    <?php
+                   $dataSesi = session();
+                   $sukses = $dataSesi->getTempdata('sukses');
+                   $fail = $dataSesi->getTempdata('fail');
+
+                   if (isset($sukses)): ?>
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <?= $sukses ?>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                    aria-hidden="true">&times;</span></button>
+                        </div>
+                    <?php endif;
+
+                   if (isset($fail)): ?>
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <?= $fail ?>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                                    aria-hidden="true">&times;</span></button>
+                        </div>
+                    <?php endif; ?>
+                    <?php if (isset($validasi)): ?>
+                        <div class="text-danger">
+                            <?= $validasi->listErrors() ?>
+                        </div>
+                    <?php endif; ?>
+                    <!--  -->
                     <div class="card-footer text-right">
                         <button class="btn btn-success">Update Nama</button>
                     </div>
