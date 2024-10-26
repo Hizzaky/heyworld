2a263b55bf35a67fed500cd280875d87681ca5c0
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 12:51:51 +0900

    update

--- ci4/app/Views/layout/default/main.php
+++ ci4/app/Views/layout/default/main.php
@@ -26,12 +26,12 @@
         <div class="main-wrapper">
             <div class="navbar-bg"></div>
             <nav class="navbar navbar-expand-lg main-navbar">
-                <ul class="navbar-nav mr-3 mr-auto">
+                <div class="navbar-nav mr-3 mr-auto">
                     <!-- <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
                                 class="fas fa-bars"></i></a></li>
                     <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
                                 class="fas fa-search"></i></a></li> -->
-                </ul>
+                </div>
                 <!-- <form class="form-inline mr-auto">
                     <div class="search-element">
                         <input class="form-control" type="search" placeholder="Search" aria-label="Search"
