50873eb2e13569d068abe593e8db9459e42a031e
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-26 12:49:31 +0900

    update

--- ci4/app/Views/layout/default/main.php
+++ ci4/app/Views/layout/default/main.php
@@ -26,7 +26,7 @@
         <div class="main-wrapper">
             <div class="navbar-bg"></div>
             <nav class="navbar navbar-expand-lg main-navbar">
-                <ul class="navbar-nav mr-3 form-inline">
+                <ul class="navbar-nav mr-3 form-inline mr-auto">
                     <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
                                 class="fas fa-bars"></i></a></li>
                     <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
