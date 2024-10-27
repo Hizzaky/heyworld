cd3989fcd7fbc1e965e0a35dc5e91059dd993f88
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-27 15:33:35 +0900

    update

--- ci4/app/Views/komponen/sidebar/prodi.php
+++ ci4/app/Views/komponen/sidebar/prodi.php
@@ -15,16 +15,18 @@
                     <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
                 </ul>
             </li>
-            <li class="menu-header">Starter</li>
+            <li class="menu-header">Kata Kerja Operasional</li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
-                    <span>Layout</span></a>
+                    <span>Menu</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
                     <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
                     <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
                 </ul>
             </li>
+            <li class="active"><a class="nav-link" href="#"><i class="far fa-square"></i>
+                    <span>Taxonomi Bloom</span></a></li>
             <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i>
                     <span>Blank Page</span></a></li>
             <li class="nav-item dropdown">
