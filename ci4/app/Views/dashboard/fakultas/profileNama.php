99b2ba99e017aa1ca341d61056b9da0032f59559
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-27 12:46:09 +0900

    update

--- ci4/app/Views/dashboard/fakultas/profileNama.php
+++ ci4/app/Views/dashboard/fakultas/profileNama.php
@@ -64,7 +64,7 @@
                     <div class="card-body">
                         <!-- <div class="row"> -->
                         <div class="form-group col-md-12 col-12">
-                            <label>Nama baru <?= $jenis_user ?></label>
+                            <label>Nama <?= $jenis_user ?></label>
                             <input type="text" class="form-control" value="<?= $login['nama_user'] ?>" disabled>
                         </div>
                         <div class="form-group col-md-12 col-12">
