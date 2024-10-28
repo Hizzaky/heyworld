f555c8907c1f0e95b7f6460f5c6522ff2e0ab3d1
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-28 14:40:22 +0900

    update

--- ci4/app/Models/Dashboard/Prodi/KataKerjaModel.php
+++ ci4/app/Models/Dashboard/Prodi/KataKerjaModel.php
@@ -12,7 +12,7 @@ class KataKerjaModel extends Model
     {
         $data = [
             'meta' => 'Kata Kerja Taxonomi Bloom',
-            'header' => 'Kata Kerja Taxonomi Bloom' 
+            'header' => 'Kata Kerja Taxonomi Bloom'
         ];
         return $data;
     }
@@ -27,7 +27,8 @@ class KataKerjaModel extends Model
         }
         return $new;
     }
-    public function templateTbl(){
+    public function templateTbl()
+    {
         $template = [
             'table_open' => '<table class="table table-responsive table-striped table-md" border="0" cellpadding="4" cellspacing="0">',
 
@@ -90,46 +91,82 @@ class KataKerjaModel extends Model
             $data[$count]['no'] = $no;
 
 
+            //             if (empty($dataC2[$i]['katalog'])) {
+//                 $dataC2[$i]['katalog'] = '';
+//             } else {
+//                 $data[$count]['c2'] = $dataC2[$i]['katalog'];
+//             }
+//             if (empty($dataC3[$i]['katalog'])) {
+//                 $dataC3[$i]['katalog'] = 'asd';
+//             } else {
+//                 $data[$count]['c3'] = $dataC3[$i]['katalog'];
+//             }
+//             if (empty($dataC4[$i]['katalog'])) {
+//                 $dataC4[$i]['katalog'] = '';
+//             } else {
+//                 $data[$count]['c4'] = '
+// <div class="dropdown">
+//   <button class="btn " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//     '.$dataC4[$i]['katalog'].'
+//   </button>
+//   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="width:10px !important; text-align:center;"> 
+//     <a class="btn btn-warning btn-sm " href="edit/' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-pencil-alt"></i> </a> | 
+//     <a class="btn btn-danger btn-sm " href="hapus-index/' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-trash"></i></a>
+//   </div>
+// </div>
+//                 ';
+//             }
+//             if (empty($dataC5[$i]['katalog'])) {
+//                 $dataC5[$i]['katalog'] = '';
+//             } else { 
+//                 $data[$count]['c5'] = '
+//                     <button  data-toggle="dropdown" class="btn dropdown nav-link dropdown-toggle >
+//                             <div class="d-sm-none d-lg-inline-block">' . $dataC5[$i]['katalog'] . '</div>
+
+            //                         <div class="dropdown-menu " >
+
+            //                             <a href="' . $dataC5[$i]['taxbloom_id'] . '" class="btn btn-warning btn-sm "><i class="fas fa-pencil-alt"></i></a>
+//                             | <a href="' . $dataC5[$i]['taxbloom_id'] . '" class="btn btn-danger btn-sm "><i class="fas fa-trash"></i></a>
+
+            //                         </div>
+//                     </button>
+//                 '; 
+//             }
+//             if (empty($dataC6[$i]['katalog'])) {
+//                 $dataC6[$i]['katalog'] = '';
+//             } else {
+//                 $data[$count]['c6'] = $dataC6[$i]['katalog'] . '<br>
+//                     <a href="' . $dataC6[$i]['taxbloom_id'] . '" class="btn btn-warning btn-sm"><i class="fas fa-pencil-alt"></i></a>
+//                     <a href="' . $dataC6[$i]['taxbloom_id'] . '" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>';
+//             }
+
+
+
             if (empty($dataC2[$i]['katalog'])) {
                 $dataC2[$i]['katalog'] = '';
             } else {
                 $data[$count]['c2'] = $dataC2[$i]['katalog'];
             }
             if (empty($dataC3[$i]['katalog'])) {
-                $dataC3[$i]['katalog'] = 'asd';
+                $dataC3[$i]['katalog'] = '';
             } else {
                 $data[$count]['c3'] = $dataC3[$i]['katalog'];
             }
             if (empty($dataC4[$i]['katalog'])) {
                 $dataC4[$i]['katalog'] = '';
             } else {
-                $data[$count]['c4'] = $dataC4[$i]['taxbloom_id'];
+                $data[$count]['c4'] = $dataC4[$i]['katalog'];
             }
             if (empty($dataC5[$i]['katalog'])) {
                 $dataC5[$i]['katalog'] = '';
-            } else { 
-                $data[$count]['c5'] = '
-                    <button  data-toggle="dropdown" class="btn dropdown nav-link dropdown-toggle >
-                            <div class="d-sm-none d-lg-inline-block">' . $dataC5[$i]['katalog'] . '</div>
-                        
-                        <div class="dropdown-menu " >
-                            
-                            <a href="' . $dataC5[$i]['taxbloom_id'] . '" class="btn btn-warning btn-sm "><i class="fas fa-pencil-alt"></i></a>
-                            | <a href="' . $dataC5[$i]['taxbloom_id'] . '" class="btn btn-danger btn-sm "><i class="fas fa-trash"></i></a>
-                            
-                        </div>
-                    </button>
-                '; 
+            } else {
+                $data[$count]['c5'] = $dataC5[$i]['katalog'];
             }
             if (empty($dataC6[$i]['katalog'])) {
                 $dataC6[$i]['katalog'] = '';
             } else {
-                $data[$count]['c6'] = $dataC6[$i]['katalog'] . '<br>
-                    <a href="' . $dataC6[$i]['taxbloom_id'] . '" class="btn btn-warning btn-sm"><i class="fas fa-pencil-alt"></i></a>
-                    <a href="' . $dataC6[$i]['taxbloom_id'] . '" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>';
+                $data[$count]['c6'] = $dataC6[$i]['katalog'];
             }
-
-
 
 
 
