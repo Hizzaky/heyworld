2c5a61f4ebe46ea56815e1692f04a6fcd89f8f38
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-28 13:03:23 +0900

    update

--- ci4/app/Models/Dashboard/Prodi/Prodi.php
+++ ci4/app/Models/Dashboard/Prodi/Prodi.php
@@ -95,7 +95,6 @@ class Prodi extends Model
     '.$dataC4[$i]['katalog'].'
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-pencil-alt"></i</a>
     <a class="dropdown-item" href="' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-trash"></i></a>
     <a class="dropdown-item" href="#">Something else here</a>
   </div>
