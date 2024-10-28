15d98fce93926887e912abce926ce14bfe6ef4c8
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-28 13:00:29 +0900

    update

--- ci4/app/Models/Dashboard/Prodi/Prodi.php
+++ ci4/app/Models/Dashboard/Prodi/Prodi.php
@@ -98,6 +98,8 @@ class Prodi extends Model
     <a class="dropdown-item" href="' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-pencil-alt"></i</a>
     <a class="dropdown-item" href="' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-trash"></i></a>
     <a class="dropdown-item" href="#">Something else here</a>
+    <a class="" href="' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-pencil-alt"></i</a>
+    <a class="" href="' . $dataC4[$i]['taxbloom_id'] . '"><i class="fas fa-trash"></i></a>
   </div>
 </div>
                 ';
