bca690df727986a57d397116f2b3849f591ffbfd
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-28 12:32:48 +0900

    update

--- ci4/app/Views/dashboard/prodi/home.php
+++ ci4/app/Views/dashboard/prodi/home.php
@@ -7,7 +7,16 @@
     </div>
     <div class="section-body">
 
-
+<div class="dropdown-center">
+  <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Centered dropdown
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Action two</a></li>
+    <li><a class="dropdown-item" href="#">Action three</a></li>
+  </ul>
+</div>
     </div>
 </section>
 
