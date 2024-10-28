f2c409f02b818997f6b8bc346b392da4c4b6d3e7
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-28 15:27:06 +0900

    update

--- ci4/app/Views/dashboard/prodi/taxbloom.php
+++ ci4/app/Views/dashboard/prodi/taxbloom.php
@@ -6,10 +6,30 @@
         <h1><?= $header_title ?></h1>
     </div>
     <div class="section-body">
-        alert
+        <!-- Notif -->
+        <?php
+        $dataSesi = session();
+        $sukses = $dataSesi->getFlashdata('sukses');
+        $fail = $dataSesi->getFlashdata('fail');
+
+        if (isset($sukses)): ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <?= $sukses ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+        <?php endif;
+        if (isset($fail)): ?>
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <?= $fail ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+        <?php endif; ?>
+        <!--  -->
         <div class="card-body p-0">
             <div class="table-responsive">
-                
+
                 <?= $table->generate($taxbloom) ?>
 
             </div>
