621005b35675b4cb0dd33c68ab3ea6bf62b3a39b
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-28 12:19:03 +0900

    update

--- ci4/app/Models/Dashboard/Prodi/Prodi.php
+++ ci4/app/Models/Dashboard/Prodi/Prodi.php
@@ -94,7 +94,20 @@ class Prodi extends Model
             if (empty($dataC5[$i]['katalog'])) {
                 $dataC5[$i]['katalog'] = '';
             } else {
-                $data[$count]['c5'] = $dataC5[$i]['katalog'];
+                $data[$count]['c5'] = $dataC5[$i]['katalog'].'
+                <div class="btn-group">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Action
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Separated link</a>
+  </div>
+</div>
+                ';
             }
             if (empty($dataC6[$i]['katalog'])) {
                 $dataC6[$i]['katalog'] = '';
