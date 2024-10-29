ff8877b3395d7322ea67e9b33d3e236431c20cf7
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-29 16:54:17 +0900

    update

--- ci4/app/Views/komponen/sidebar/prodi.php
+++ ci4/app/Views/komponen/sidebar/prodi.php
@@ -7,7 +7,7 @@
             <a href="/<?= $jenis_user ?>">SU</a>
         </div>
         <ul class="sidebar-menu active">
-            <li class="menu-header">Dashboard</li>
+            <li class="menu-header">Dashboardd</li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
