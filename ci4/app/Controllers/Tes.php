06dfb447bacde3e39cf3de3c25f3d11c227b1ca3
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-29 16:29:18 +0900

    update

--- ci4/app/Controllers/Tes.php
+++ ci4/app/Controllers/Tes.php
@@ -8,6 +8,6 @@ class Tes extends BaseController
     {
         // return redirect()->to('Homepage/');
 
-        return view('tes/home');
+        return view('tes/layout_default');
     }
 }
--- ci4/app/Views/layout/default/main.php
+++ ci4/app/Views/layout/default/main.php
@@ -59,6 +59,7 @@
     <script src="<?= base_url() ?>/template/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="<?= base_url() ?>/template/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <script src="<?= base_url() ?>/template/node_modules/jquery.nicescroll/dist/jquery.nicescroll.min.js"></script>
+    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
         crossorigin="anonymous"></script>
