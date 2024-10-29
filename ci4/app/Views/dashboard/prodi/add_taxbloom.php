1c5e1dc988b8f9db36ffe8c91f9684f65f03a387
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-29 09:54:50 +0900

    update

--- ci4/app/Views/dashboard/prodi/add_taxbloom.php
+++ ci4/app/Views/dashboard/prodi/add_taxbloom.php
@@ -37,7 +37,7 @@
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kode </label>
                             <div class="col-sm-12 col-md-7">
-                                <select class="form-control selectric" name="kode" required>
+                                <select class="form-control selectric" name="kode" required <?= $x = $val['kode'] == 'C4' ? 'disabled' : '' ?>>
                                     <?php
                                     $x = 0;
                                     foreach ($kode as $val): ?>
