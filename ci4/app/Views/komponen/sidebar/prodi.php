2fc0adeb28b12e2c0030d49e21fc1defd3e6c161
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-29 18:51:23 +0900

    update

--- ci4/app/Views/komponen/sidebar/prodi.php
+++ ci4/app/Views/komponen/sidebar/prodi.php
@@ -8,106 +8,24 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="nav-item dropdown ">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
-                    <li class=""><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                    <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-                </ul>
-            </li>
+            <li class="/Prodi"><a class="nav-link" href="#"><i class="far fa-house"></i>
+                    <span>Home</span></a></li>
+
             <li class="menu-header">Kata Kerja Operasional</li>
-            <li class="nav-item dropdown <?= $var=isset($sideTaxbloom)? 'active':'' ?>">
+            <li class="nav-item dropdown <?= $var = isset($sideTaxbloom) ? 'active' : '' ?>">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
                     <span>Taxonomi Bloom</span></a>
                 <ul class="dropdown-menu">
-                    <li class="<?= $var = isset($menuIndexKataKerja) ? 'active' : '' ?>"><a class="nav-link" href="/Prodi/Kata-kerja">Index Kata Kerja</a></li>
-                    <li class="<?= $var = isset($menuAddKataKerja) ? 'active' : '' ?>"><a class="nav-link" href="/Prodi/Penambahan-kata-kerja">Penambahan Kata Kerja</a></li>
-                    <li class="<?= $var = isset($menuDelKataKerja) ? 'active' : '' ?>"><a class="nav-link" href="/Prodi/Restore-kata-kerja">Kata Kerja Terhapus</a></li>
+                    <li class="<?= $var = isset($menuIndexKataKerja) ? 'active' : '' ?>"><a class="nav-link"
+                            href="/Prodi/Kata-kerja">Index Kata Kerja</a></li>
+                    <li class="<?= $var = isset($menuAddKataKerja) ? 'active' : '' ?>"><a class="nav-link"
+                            href="/Prodi/Penambahan-kata-kerja">Penambahan Kata Kerja</a></li>
+                    <li class="<?= $var = isset($menuDelKataKerja) ? 'active' : '' ?>"><a class="nav-link"
+                            href="/Prodi/Restore-kata-kerja">Kata Kerja Terhapus</a></li>
                     <li><a class="nav-link" href="#">Menu</a></li>
                 </ul>
             </li>
-            <li class=""><a class="nav-link" href="#"><i class="far fa-square"></i>
-                    <span>Menu</span></a></li>
-            <li ><a class="nav-link" href="blank.html"><i class="far fa-square"></i>
-                    <span>Blank Page</span></a></li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
-                    <span>Bootstrap</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
-                    <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
-                </ul>
-            </li>
-            <li class="menu-header">MENU</li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i>
-                    <span>Components</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="components-article.html">Article</a></li>
-                    <li><a class="nav-link beep beep-sidebar" href="components-avatar.html">Avatar</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i>
-                    <span>Forms</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="forms-advanced-form.html">Advanced Form</a></li>
-                    <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google
-                        Maps</span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="gmaps-advanced-route.html">Advanced Route</a></li>
-                    <li><a href="gmaps-draggable-marker.html">Draggable Marker</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-plug"></i>
-                    <span>Modules</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="modules-calendar.html">Calendar</a></li>
-                    <li><a class="nav-link" href="modules-chartjs.html">ChartJS</a></li>
-                </ul>
-            </li>
-            <li class="menu-header">MENU</li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="far fa-user"></i> <span>Auth</span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="auth-forgot-password.html">Forgot Password</a></li>
-                    <li><a href="auth-login.html">Login</a></li>
-                    <li><a class="beep beep-sidebar" href="auth-login-2.html">Login 2</a></li>
-                    <li><a href="auth-register.html">Register</a></li>
-                    <li><a href="auth-reset-password.html">Reset Password</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-exclamation"></i>
-                    <span>Errors</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="errors-503.html">503</a></li>
-                    <li><a class="nav-link" href="errors-403.html">403</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-bicycle"></i>
-                    <span>Features</span></a>
-                <ul class="dropdown-menu">
-                    <li><a class="nav-link" href="features-activities.html">Activities</a></li>
-                    <li><a class="nav-link" href="features-post-create.html">Post Create</a></li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-ellipsis-h"></i>
-                    <span>Utilities</span></a>
-                <ul class="dropdown-menu">
-                    <li><a href="utilities-contact.html">Contact</a></li>
-                    <li><a class="nav-link" href="utilities-invoice.html">Invoice</a></li>
-                </ul>
-            </li>
-            <li><a class="nav-link" href="credits.html"><i class="fas fa-pencil-ruler"></i>
-                    <span>Credits</span></a></li>
+
         </ul>
 
     </aside>
