7c75f0ab20badd9ee8737b85e6ecbcd98086d081
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-29 16:07:24 +0900

    update

--- ci4/app/Views/komponen/sidebar/fakultas.php
+++ ci4/app/Views/komponen/sidebar/fakultas.php
@@ -25,7 +25,7 @@
                     <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
                 </ul>
             </li>
-            <li class="active"><a class="nav-link" href="blank.html"><i class="far fa-square"></i>
+            <li ><a class="nav-link" href="blank.html"><i class="far fa-square"></i>
                     <span>Blank Page</span></a></li>
             <li class="nav-item dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i>
