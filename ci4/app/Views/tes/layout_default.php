3945982bcb1b7c3f7efd8ccb76f36d62a96ef82c
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-29 16:32:33 +0900

    update

--- ci4/app/Views/tes/layout_default.php
+++ ci4/app/Views/tes/layout_default.php
@@ -429,7 +429,7 @@
             <div class="main-content">
                 <section class="section">
                     <div class="section-header">
-                        <h1>Default Layout</h1>
+                        <h1>Default Layout 111</h1>
                         <div class="section-header-breadcrumb">
                             <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                             <div class="breadcrumb-item"><a href="#">Layout</a></div>
