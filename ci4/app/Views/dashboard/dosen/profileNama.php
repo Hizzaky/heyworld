5ea7edfcee1685791546b320ed345ccf44ea8946
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-30 11:48:50 +0900

    update

--- ci4/app/Views/dashboard/dosen/profileNama.php
+++ ci4/app/Views/dashboard/dosen/profileNama.php
@@ -6,7 +6,7 @@
 
     <h2 class="section-title">Hi, <?= $nama_user ?>!</h2>
     <p class="section-lead">
-        ( <?= $nidn ?> )
+        ( <?= $login['nidn'] ?> )
     </p>
 
     <div class="row mt-sm-4">
