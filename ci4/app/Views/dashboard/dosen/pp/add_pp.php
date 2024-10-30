49389408f8a5aeca853d569d9f1079c9efb6ac0d
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-30 17:43:57 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -38,20 +38,14 @@
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kode </label>
                             <div class="col-sm-12 col-md-7">
                                 <select class="form-control selectric" name="kode" required <?= $x = isset($disabled) ? 'disabled' : '' ?>>
-                                    <?php
-                                    $x = 0;
-                                    foreach ($kode as $val): ?>
-                                        <option <?= $selected ?> value="<?= $val['kode'] ?>"><?= $val['kode'] ?></option>
-                                        <?php 
-                                        $x++;
-                                    endforeach; ?>
+                                    
                                 </select>
                             </div>
                         </div>
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kata Kerja</label>
                             <div class="col-sm-12 col-md-7">
-                                <input type="text" class="form-control" name="katalog" required value="<?= $val=isset($taxbloom['katalog']) ? $taxbloom['katalog'] : '' ?>">
+                                <input type="text" class="form-control" name="katalog" required value="">
                                 <div class="invalid-feedback">
                                     Inputkan kata kerja baru dengan benar!
                                 </div>
