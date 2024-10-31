497c37675bb3891dedcf79b92ee088e1cbe6787f
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 23:05:27 +0900

    update

--- ci4/app/Models/Dashboard/Dosen/PpModel.php
+++ ci4/app/Models/Dashboard/Dosen/PpModel.php
@@ -105,7 +105,7 @@ class PpModel extends Model
 
         $ret = '
             <div class="dropdown">
-                <button class="btn " type="button" onclick="modalKataKerja('.$dir.''.$katalog.')">' . $data['katalog'] . ' 
+                <button class="btn " type="button" onclick="modalKataKerja('.$dir.','.$katalog.')">' . $data['katalog'] . ' 
                 </button>
             </div>
         ';
