49418eff9764567eefc3d581222da42822b11560
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 18:37:14 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -87,7 +87,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalKataKerjaLabel">Modal title</h5>
+                <h5 class="modal-title" id="modalKataKerjaLabel">Kata Kerja Taxonomi Bloom</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -98,10 +98,10 @@
                     <?= $table->generate($taxbloom) ?>
                 </div>
             </div>
-            <div class="modal-footer">
+            <!-- <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+            </div> -->
         </div>
     </div>
 </div>
