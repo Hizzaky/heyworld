654810e7f305fe7b8f2ddc276652050fd5047a8b
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 13:02:47 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -41,7 +41,7 @@
                                 <!-- <button class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
                                     data-target="#modalKataKerja" placeholder="--Pilih Kata Kerja--" value="" 
                                     readonly required><i class="fas fa-list-ul"></i>add</button> -->
-                                <input class="btn form-control" type="text" placeholder="--Pilih Kata Kerja--" value="" required>
+                                <input class="btn form-control" type="text" placeholder="--Pilih Kata Kerja--" value="" required disaabled>
                                  <!-- <a class="btn btn-ouline-info  " data-toggle="modal" data-target="#modalKataKerja"> 
                                     <i class="fas fa-list"></i></a> --> 
 
