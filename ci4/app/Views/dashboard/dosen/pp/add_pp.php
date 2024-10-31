d2007f3ef788cc856e634406662738abcfbf6526
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 23:49:19 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -39,7 +39,7 @@
                     <form method="testing" class="needs-validation" novalidate>
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kode </label>
-                            <div class="col-sm-12 col-md-7 form-inline">
+                            <div class="col-sm-12 col-md-7 ">
 
                                 <!-- <button class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
                                     data-target="#modalKataKerja" placeholder="--Pilih Kata Kerja--" value="" 
