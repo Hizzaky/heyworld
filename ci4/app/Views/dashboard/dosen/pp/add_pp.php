f6ba1ebcd219d7c1d53276d8b0c8773fcc603c4a
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 23:53:49 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -35,7 +35,7 @@
                 <div class="card-body">
 
                 <!-- <button class="btn btn-danger" type="button" onclick="modalKataKerja(  )">button</button>  -->
-
+asdasdasd
                     <form method="testing" class="needs-validation" novalidate>
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kode </label>
