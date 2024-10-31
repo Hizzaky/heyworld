a2f88af1674a687c81c6cde9556bb0d0f97633cb
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 11:51:34 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -38,11 +38,10 @@
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kode </label>
                             <div class="col-sm-12 col-md-7">
                                 
-                                <!-- <input class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
+                                <button class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
                                     data-target="#modalKataKerja" placeholder="--Pilih Kata Kerja--" value="" 
-                                    readonly required> -->
-                                <input class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
-                                    data-target="#modalKataKerja" placeholder="--Pilih Kata Kerja--" value="" 
+                                    readonly required><i class=""></i>add</button>
+                                <input class="" type="text" placeholder="--Pilih Kata Kerja--" value="" 
                                      required>
                                 <div class="invalid-feedback"> 
                                     ----------------!
