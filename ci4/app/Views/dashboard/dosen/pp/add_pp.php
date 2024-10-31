52430208b32c588fb0aa36a9021cde3f1676535a
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 23:50:07 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -39,7 +39,7 @@
                     <form method="testing" class="needs-validation" novalidate>
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kode </label>
-                            <div class="col-sm-12 col-md-7 ">
+                            <div class="col-sm-12 col-md-7 form-inline">
 
                                 <!-- <button class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
                                     data-target="#modalKataKerja" placeholder="--Pilih Kata Kerja--" value="" 
@@ -52,12 +52,12 @@
 
                                 <!-- <input class="btn form-control" type="text" name="red" id="katalog"
                                     placeholder="--Pilih Kata Kerja--" value="" required style="color:red" > -->
+                                    
+                                    <div class="invalid-feedback">
+                                        Silahkan pilih kata kejra!
+                                    </div>
                                     <input class="btn form-control" type="text" name="taxbloom_id" id="id"
                                         placeholder="ID" value="" required hidden >
-
-                                <div class="invalid-feedback">
-                                    Silahkan pilih kata kejra!
-                                </div>
 
 
                             </div> 
