49d2e98ed78b5635ad946fddaba98736425e460c
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 18:51:46 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -75,7 +75,7 @@
                         </div>
                         <div class="card-footer text-right">
                             <button class="btn btn-primary">
-                                6
+                                7
                             </button>
                         </div>
                     </form>
@@ -97,7 +97,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <div style="height:750vh; overflow:auto">
+                <div style="height:75vh; overflow:auto">
 
                     <?= $table->generate($taxbloom) ?>
                 </div>
