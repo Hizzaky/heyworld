be2a40c402dd6a45742adb5d7217b98515030e11
Hizzaky <zacky66blackcloud@gmailcom>
2024-10-31 11:44:55 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/add_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/add_pp.php
@@ -37,11 +37,13 @@
                         <div class="form-group row mb-4">
                             <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Kode </label>
                             <div class="col-sm-12 col-md-7">
-                                <!-- <select class="form-control selectric" name="kode" required <?= $x = isset($disabled) ? 'disabled' : '' ?>>
-                                </select> -->
+                                
+                                <!-- <input class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
+                                    data-target="#modalKataKerja" placeholder="--Pilih Kata Kerja--" value="" 
+                                    readonly required> -->
                                 <input class="btn btn-outline-secondary form-control" type="text" data-toggle="modal"
                                     data-target="#modalKataKerja" placeholder="--Pilih Kata Kerja--" value="" 
-                                    readonly required>
+                                     required>
                                 <div class="invalid-feedback">
                                     ----------------!
                                 </div>
