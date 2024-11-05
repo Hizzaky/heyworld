615b4b31dedd03045a300df6756b78a8441f1804
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-05 23:33:58 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/home.php
+++ ci4/app/Views/dashboard/dosen/pp/home.php
@@ -37,8 +37,8 @@
         <div class="row">
             <?php foreach($pp as $key=>$val): ?>
                 
-                <div class="container" style="text-align:center">
                 <div class="card col-12 col-sm-6 col-md-3" style="margin:3px">
+                    <div class="container" style="">
                         <div class="card-body">
                             <h5 class="card-title"><?= $val['katalog'] ?></h5>
                             <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
