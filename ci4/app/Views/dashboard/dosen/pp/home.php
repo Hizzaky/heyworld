97df7a0c9f9c8fd6fb6afc2cfc0d9a7eb0e8f692
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-05 22:58:54 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/home.php
+++ ci4/app/Views/dashboard/dosen/pp/home.php
@@ -42,7 +42,7 @@
                         <div class="card-body">
                             <h5 class="card-title"><?= $val['katalog'] ?></h5>
                             <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                            <p class="card-text " style="max-width: 20ch;">
+                            <p class="card-text " style="max-width: 20ch !important;">
                                 <span style="color:red">Mampu <?= $val['katalog'] ?></span>
                                 <span style="color:blue"><?= $val['blue'] ?></span>
                                 <span style="color:green"><?= $val['green'] ?></span>
