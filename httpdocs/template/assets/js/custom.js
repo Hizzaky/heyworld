f21aca659f8b30605691e806904660d098df5c75
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-05 23:03:52 +0900

    update

--- httpdocs/template/assets/js/custom.js
+++ httpdocs/template/assets/js/custom.js
@@ -49,6 +49,7 @@ function inputPp() {
     document.getElementById('blank').innerHTML = '';
 }
 
+limitText();
 function limitText() {
     var para = document.getElementsByClassName("long-text")[0];
     var text = para.innerHTML;
