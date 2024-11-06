e61e28fb6f750187ba645541e79da26ff64ab956
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-06 18:03:07 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/home.php
+++ ci4/app/Views/dashboard/dosen/pp/home.php
@@ -84,7 +84,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalKataKerjaLabel">
-                    Penguasaan Pengetahuan <span id="modalpp"></span>
+                    Penguasaan Pengetahuan (<span id="modalpp"></span>)
                 </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
