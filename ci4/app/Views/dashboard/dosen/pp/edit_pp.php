b88ad967a1e22ebe9e604566f7f2d05348b6ece6
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-06 23:44:40 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/edit_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/edit_pp.php
@@ -36,7 +36,7 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-2"></div>
-                            <div class=" col-md-9 col-" style="border-style:double; padding:10px">
+                            <div class=" col-md-9 col-" style="border-style:double; padding:10px" style="pointer:default">
                                 <span id="tred" style="color:red">Mampu <?= $edit[0]['katalog'] ?></span>
                                 <span id="tblue" style="color:blue"><?= $edit[0]['blue'] ?></span>
                                 <span id="tgreen" style="color:green"><?= $edit[0]['green'] ?></span>
