89f093a4da17c26059430240b24bf67a184db490
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-06 23:45:27 +0900

    update

--- ci4/app/Views/dashboard/dosen/pp/edit_pp.php
+++ ci4/app/Views/dashboard/dosen/pp/edit_pp.php
@@ -36,11 +36,13 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-2"></div>
-                            <div class=" col-md-9 col-" style="border-style:double; padding:10px" style="pointer:default">
-                                <span id="tred" style="color:red">Mampu <?= $edit[0]['katalog'] ?></span>
-                                <span id="tblue" style="color:blue"><?= $edit[0]['blue'] ?></span>
-                                <span id="tgreen" style="color:green"><?= $edit[0]['green'] ?></span>
-                                <!-- <span id="blank">Silahkan inputkan kata kerja dibawah!</span> -->
+                            <div class=" col-md-9 col-" style="border-style:double; padding:10px" >
+                                <p style="pointer:default">
+                                    <span id="tred" style="color:red">Mampu <?= $edit[0]['katalog'] ?></span>
+                                    <span id="tblue" style="color:blue"><?= $edit[0]['blue'] ?></span>
+                                    <span id="tgreen" style="color:green"><?= $edit[0]['green'] ?></span>
+                                    <!-- <span id="blank">Silahkan inputkan kata kerja dibawah!</span> -->
+                                </p>
                             </div>
                         </div>
                     </div><br>
