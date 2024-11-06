4b3621e8f2d4f847ece38f0692e6b4dff85b289c
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-06 18:14:07 +0900

    update

--- ci4/app/Models/CustomModel.php
+++ ci4/app/Models/CustomModel.php
@@ -68,7 +68,7 @@ class CustomModel
         $builder->select('*');
         $builder->join('t_taxbloom', 't_taxbloom.taxbloom_id=t_pp.taxbloom_id');
         $builder->join('t_dosen','t_dosen.dosen_id=t_pp.dosen_id');
-        $builder->where('dosen_id',$id);
+        $builder->where('t_pp.dosen_id',$id);
 
 
 
