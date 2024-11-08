3c307bdaf1cfb510f74ddf95eb6c1c47ff99038d
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-08 12:43:11 +0900

    update

--- ci4/app/Views/komponen/data/modalTaxbloom.php
+++ ci4/app/Views/komponen/data/modalTaxbloom.php
@@ -2,15 +2,14 @@
     <div class="modal-dialog " role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalKataKerjaLabel">tes
+                <h5 class="modal-title" id="modalKataKerjaLabel">
                     <?= $sub_title ?>
                     (<span id="modalPp"></span>)
                 </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-            </div>
-            <hr>
+            </div><br>
             <div class="modal-body" style="padding:5px;">
                 <div class="container">
                     <p class="card-text" style="text-align:justify">
