48da885c4c4c021219472f35780416e36d9acc69
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-08 12:55:13 +0900

    update

--- ci4/app/Views/komponen/load/modalTaxbloomTbl.php
+++ ci4/app/Views/komponen/load/modalTaxbloomTbl.php
@@ -2,14 +2,14 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modalKataKerjaLabel"><?= $modal_title ?></h5>
+                <h5 class="modal-title" id="modalKataKerjaLabel">terbaru <?= $modal_title ?></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <div style="height:75vh; overflow:auto">
-                    <?= $table->generate($taxbloom) ?>
+                    <?= $table ?>
                 </div>
             </div>
         </div>
