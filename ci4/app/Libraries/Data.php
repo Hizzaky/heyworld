83dd81a72ddecfe434adb12c328605f1021c0a5f
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-08 12:45:51 +0900

    update

--- ci4/app/Libraries/Data.php
+++ ci4/app/Libraries/Data.php
@@ -2,16 +2,16 @@
 
 class Data{
     public function alert($params) {
-        return view('komponen/data/alert',$params);
+        return view('komponen/load/alert',$params);
     }
     public function loop() {
-        return view('komponen/data/loop');
+        return view('komponen/load/loop');
     }
     public function modalTbl() {
-        return view('komponen/data/modalTbl');
+        return view('komponen/load/modalTbl');
     }
     public function modalTaxbloomKonteks() {
-        return view('komponen/data/modalTaxbloomKonteks');
+        return view('komponen/load/modalTaxbloomKonteks');
     }
 }
 
