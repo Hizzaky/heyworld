65c8de8b30a3f3feafce1f32ffdefbf74548f21a
Hizzaky <zacky66blackcloud@gmailcom>
2024-11-13 14:05:30 +0900

    update

--- ci4/app/Models/Dashboard/Prodi/Pp/RestorePpModel.php
+++ ci4/app/Models/Dashboard/Prodi/Pp/RestorePpModel.php
@@ -80,11 +80,11 @@ class RestorePpModel extends Model
                     <div style="white-space:nowrap;">
                         <button class="btn btn-primary " 
                         data-confirm="Restore Kata Kerja?|Gunakan kembali kata kerja?" 
-                        data-confirm-yes="prodiRestoreTaxbloom(' . $val['pp_delete_id'] . ')"
+                        data-confirm-yes="prodiRestorePp(' . $val['pp_delete_id'] . ')"
                         ><i class="fas fa-undo"></i></button>
                         <button class="btn btn-danger btn-sm " 
                         data-confirm="Hapus Kata Kerja?|Yakin ingin menghapus kata kerja secara permanen?" 
-                        data-confirm-yes="prodiDeleteTaxbloomPermanen(' . $val['pp_delete_id'] . ')"
+                        data-confirm-yes="prodiDeletePpPermanen(' . $val['pp_delete_id'] . ')"
                         ><i class="fas fa-trash"></i></button>
                     </div>
             '
