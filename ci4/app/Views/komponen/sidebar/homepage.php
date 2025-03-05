05f8d554a2702a4d63c406ee19f25f5e455b68ab
Hizzaky <37980872+Hizzaky@users.noreply.github.com>
2025-03-05 14:13:42 +0900

    tes update

--- ci4/app/Views/komponen/sidebar/homepage.php
+++ ci4/app/Views/komponen/sidebar/homepage.php
@@ -2,8 +2,8 @@
     <ul class="list-group">
         <li class="list-group-item s-head">Daftar Akun</li>
 
+        <li class="list-group-item s-body">bla </li>
+        <li class="list-group-item s-body">bla bla</li>
         <li class="list-group-item s-body">bla bla bla</li>
-        <li class="list-group-item s-body">bla bla bla</li>
-        <li class="list-group-item s-body">bla bla bla 3</li>
     </ul>
 </div> 
